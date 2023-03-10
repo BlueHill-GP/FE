f599802e7e0a6e95ce409a54ba9213f7411b0685
linhanna <linh.nguyenthingoc23@student.passerellesnumeriques.org>
2023-03-10 11:30:43 +0700

    update code file Register

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { LoginData } from "../api/authApi";
 import Input from "../components/input/Input";
-import "../../src/assets/css/register.css"
 
 interface LoginState {
   login: (user: LoginData) => void;
@@ -33,29 +32,21 @@ const LoginPage = (props: LoginState) => {
   changeRoute("register")
 }
   return (
-    <div className={"container-register"}>
-        <section className={"register-section"}>
-            <div className={"form_header"}>
-                <h1 className={"form_header_welcome"}> Chao mung den Cuoidi Cuoidi</h1>
-                <p> Hay tan huong dam cuoi cua ban</p>
-                <h1 className={"form_header_register-text"}>Đăng nhập</h1>
-            </div>
-
-            <Input
-                type="text"
-                name="email"
-                value={user.email}
-                function={handleInputChange}
-              />
-            <Input
-                type="password"
-                name="password"
-                value={user.password}
-                function={handleInputChange}
-              />
-              <button onClick={handleSubmit}>Login</button>
-              <button onClick={moveRegister}>Register</button>
-        </section>
+    <div >
+      <Input
+        type="text"
+        name="email"
+        value={user.email}
+        function={handleInputChange}
+      />
+      <Input
+        type="password"
+        name="password"
+        value={user.password}
+        function={handleInputChange}
+      />
+      <button onClick={handleSubmit}>Login</button>
+      <button onClick={moveRegister}>Register</button>
     </div>
   );
 };
