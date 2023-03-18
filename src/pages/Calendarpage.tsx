e57a5597cfdc32d054d0d66bacf9559fc6062ca8
HieuDEVELOPER <hieunguyentx0010@gmail.com>
2023-03-18 14:55:47 +0700

    Add caledar in calendal page

--- src/pages/Calendarpage.tsx
+++ src/pages/Calendarpage.tsx
@@ -1,51 +1,11 @@
-import React, { useState } from "react";
-import "./EventForm.css";
+import Calendar from "react-calendar";
 
-function EventForm(props) {
-  const [title, setTitle] = useState("");
-  const [date, setDate] = useState("");
-
-  const handleTitleChange = (event) => {
-    setTitle(event.target.value);
-  };
-
-  const handleDateChange = (event) => {
-    setDate(event.target.value);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const eventObject = {
-      id: Date.now(),
-      title,
-      date,
-    };
-    props.onAddEvent(eventObject);
-    setTitle("");
-    setDate("");
-  };
-
+function Calendarpage() {
   return (
-    <form onSubmit={handleSubmit} className="event-form">
-      <label htmlFor="title">Title</label>
-      <input
-        type="text"
-        id="title"
-        value={title}
-        onChange={handleTitleChange}
-        required
-      />
-      <label htmlFor="date">Date</label>
-      <input
-        type="date"
-        id="date"
-        value={date}
-        onChange={handleDateChange}
-        required
-      />
-      <button type="submit">Add Event</button>
-    </form>
+    <div>
+      <Calendar />
+    </div>
   );
 }
 
-export default EventForm;
+export default Calendarpage;
