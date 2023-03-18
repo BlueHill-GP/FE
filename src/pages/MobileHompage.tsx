6833eb3165de42dd3fda5073537f0d873a1c06dc
HieuDEVELOPER <hieunguyentx0010@gmail.com>
2023-03-18 14:42:03 +0700

    add mobile hompage

---
+++ src/pages/MobileHompage.tsx
@@ -0,0 +1,13 @@
+import SetTime from "../components/SetTime"
+function MobileHompage() {
+  return (
+    <div>
+      MobileHompage
+      <div>
+        <SetTime />
+      </div>
+    </div>
+  );
+}
+
+export default MobileHompage
