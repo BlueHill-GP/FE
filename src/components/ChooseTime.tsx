748a61b41381bd7cd9a8682b0eb884b906ffcf0e
HieuDEVELOPER <hieunguyentx0010@gmail.com>
2023-03-20 17:26:18 +0700

    Update ui package

--- src/components/ChooseTime.tsx
+++ src/components/ChooseTime.tsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from "react";
 import { getRandomServicePackage } from "../api/servicePackage";
 import "../assets/css/chooseTime.css";
 import ServicePackage from "./ServicePackage";
+import Categories from "./Categories";
 const App: React.FC = () => {
   const [selectedDate, setSelectedDate] = useState<string | null>(null);
   const [selectedTime, setSelectedTime] = useState<string | null>(null);
@@ -51,34 +52,40 @@ const App: React.FC = () => {
   };
 
   return (
-    <div className="choose-time">
-      <div>
-        <TimePicker.RangePicker onChange={handleTimeChange} />
-        <DatePicker onChange={handleDateChange} />
-        <Space wrap>
-          <Select
-            defaultValue={selectedLocation}
-            style={{ width: 120 }}
-            onChange={handleChangeLocation}
-            options={[
-              { value: "hn", label: "Hà Nội" },
-              { value: "dn", label: "Đà Nẵng" },
-              { value: "sg", label: "Sài Gòn" },
-            ]}
-          />
-        </Space>
+    <div className="choose-time-container">
+      <div className="choose-time">
+        <div>
+          <Categories />
+        </div>
+        <div className="choose-time-search">
+          <TimePicker.RangePicker onChange={handleTimeChange} />
+          <DatePicker onChange={handleDateChange} />
+          <Space wrap>
+            <Select
+              defaultValue={selectedLocation}
+              style={{ width: 120 }}
+              onChange={handleChangeLocation}
+              options={[
+                { value: "hn", label: "Hà Nội" },
+                { value: "dn", label: "Đà Nẵng" },
+                { value: "sg", label: "Sài Gòn" },
+              ]}
+            />
+          </Space>
 
-        <Button onClick={handleSendData}>Gửi</Button>
+          <Button onClick={handleSendData}>Gửi</Button>
+        </div>
       </div>
-
-      {servicePackages &&
-        servicePackages.map((servicePackage: any, index: number) => (
-          <ServicePackage
-            servicePackage={servicePackage}
-            select={filter}
-            key={index}
-          />
-        ))}
+      <div className="show-data-after-search">
+        {servicePackages &&
+          servicePackages.map((servicePackage: any, index: number) => (
+            <ServicePackage
+              servicePackage={servicePackage}
+              select={filter}
+              key={index}
+            />
+          ))}
+      </div>
     </div>
   );
 };
