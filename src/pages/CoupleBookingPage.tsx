7dff69af26c91b0f2d248c4df1da363fa7a95d27
TaiLinhPN <linh.nguyenbuitai23@student.passerellesnumeriques.org>
2023-03-24 14:07:22 +0700

    production 3.6

--- src/pages/CoupleBookingPage.tsx
+++ src/pages/CoupleBookingPage.tsx
@@ -8,12 +8,16 @@ import { useSelector } from "react-redux";
 import { RootState } from "../redux/store";
 import { socket } from "../App";
 
+import { Image } from "antd";
+import { messageError, messageSuccess } from "../utils/notifi";
+
 interface IProp {
   user: {
     userId: string;
     name: string;
     email: string;
     userType: string;
+    avatar:string
   };
 }
 const CoupleBookingPage = (props: IProp) => {
@@ -31,6 +35,41 @@ const CoupleBookingPage = (props: IProp) => {
   }, []);
 
   const [avatar, setAvatar] = useState<File[]>([]);
+  // const handleFileChangeProfile = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   const selectedFiles = e.target.files;
+  //   if (selectedFiles && selectedFiles.length > 0) {
+  //     const file = selectedFiles[0];
+  //     setAvatar([file]);
+  //   }
+  // };
+  // const handleRemoveFile = (index: number) => {
+  //   const newFiles = [...avatar];
+  //   newFiles.splice(index, 1);
+  //   setAvatar(newFiles);
+  // };
+
+  // const handleUpdateAvatar = async () => {
+  //   const formData = new FormData();
+  //   if (avatar) {
+  //     formData.append("images", avatar[0]);
+  //   }
+
+  //   try {
+  //     const response = await updateAvatarApi(formData);
+
+  //     if (response.status === 200) {
+  //       console.log(response.data.data);
+  //       setUser(user.userId);
+  //       setAvatar([]);
+  //     } else {
+  //       console.log(response.data.message);
+  //     }
+  //   } catch (error) {
+  //     console.log("err1");
+  //     console.log(error);
+  //   }
+  // };
+
   const handleFileChangeProfile = (e: React.ChangeEvent<HTMLInputElement>) => {
     const selectedFiles = e.target.files;
     if (selectedFiles && selectedFiles.length > 0) {
@@ -57,14 +96,17 @@ const CoupleBookingPage = (props: IProp) => {
         console.log(response.data.data);
         setUser(user.userId);
         setAvatar([]);
+        messageSuccess("thêm avatar thành công");
       } else {
         console.log(response.data.message);
       }
     } catch (error) {
-      console.log("err1");
+      messageError("Thêm avatar thất bại");
+
       console.log(error);
     }
   };
+
   // useEffect(() => {
   socket.on("update-booking", (data: any) => {
     console.log(data);
@@ -80,7 +122,7 @@ const CoupleBookingPage = (props: IProp) => {
   return (
     <div className="profile-container">
       <div className="profile-page">
-        <div className="info">
+        {/* <div className="info">
           {user.avatar !== "" ? (
             <img className="avatar-profile" src={user.avatar} alt="" />
           ) : (
@@ -121,6 +163,83 @@ const CoupleBookingPage = (props: IProp) => {
             <label htmlFor="chooseImg1">
               <i className="fa-solid fa-pen-to-square"></i>
             </label>
+          </div>
+        </div> */}
+        <div className="info">
+          <div className="fixed-box">
+            <div className="info-container">
+              <div className="row justify-center col-row">
+                <div>
+                <div className="img-avatar">
+                  {user.avatar !== "" ? (
+                    <Image
+                      width={170}
+                      height={170}
+                      className="avatar-profile"
+                      src={user.avatar}
+                      alt=""
+                    />
+                  ) : (
+                    <Image
+                      className="avatar-profile"
+                      src={
+                        "https://static2.yan.vn/YanNews/2167221/202102/facebook-cap-nhat-avatar-doi-voi-tai-khoan-khong-su-dung-anh-dai-dien-e4abd14d.jpg"
+                      }
+                      alt=""
+                    />
+                  )}
+                </div>
+                {avatar.map((file, index) => (
+                  <div className="handle-img">
+                    <div key={index}>
+                      <Image
+                        className="avatar-review"
+                        width={100}
+                        height={100}
+                        src={URL.createObjectURL(file)}
+                      />
+                      <div className="row justify-center icon-handle-avatar ">
+                        <div onClick={() => handleRemoveFile(index)}>
+                          <i className="fa-solid fa-trash"></i>
+                        </div>
+                        <div onClick={handleUpdateAvatar}>
+                          <i className="fa-solid fa-check"></i>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+                </div>
+
+                <label className="update-avatar" htmlFor="chooseImg1">
+                  <i className="fa-solid fa-pen-to-square"></i>
+                </label>
+                <input
+                  className="choose-img"
+                  id="chooseImg1"
+                  type="file"
+                  onChange={handleFileChangeProfile}
+                />
+
+                <div className="profile-text">
+                  <h2 className="profile-name-text">{user.name}</h2>
+                  <h2 className="user-tyle-text">
+                    {user.userType === "makeup" ? (
+                      <i className="fa-solid fa-paintbrush"></i>
+                    ) : (
+                      <i className="fa-solid fa-camera"></i>
+                    )}
+
+                    {user.userType}
+                  </h2>
+                </div>
+
+                <div className="start-container"></div>
+                <div className="row desc-profile justify-center col-row">
+                  {user.desc ? <p>{user.desc}</p> : <p></p>}
+                </div>
+              </div>
+            </div>
           </div>
         </div>
 
